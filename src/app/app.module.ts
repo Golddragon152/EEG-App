20c554912334f8d5359b5f74bfe8cbd986427e77
Chrischy <golddragon152@googlemail.com>
2018-03-22 20:39:58 +0100

    Recognize first Anlage
    Get the first Anlage from the app user, we start with camera and marktstammdatenregister values.
    Added Material-Icons beside font awesome, to get the great new camera icon.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
-import { LaunchComponent } from './views/launch/launch.component';
+import {AppComponent} from './app.component';
+import {LaunchComponent} from './views/launch/launch.component';
 import {GlobalService} from "./services/global.service";
 import {RouterModule} from "@angular/router";
 import {ROUTES} from "./app.routes";
-import { HomeComponent } from './views/home/home.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-
+import {HomeComponent} from './views/home/home.component';
+import {HeaderComponent} from './components/header/header.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {RecognizeComponent} from './views/recognize/recognize.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +18,8 @@ import { FooterComponent } from './components/footer/footer.component';
     LaunchComponent,
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    RecognizeComponent,
   ],
   imports: [
     BrowserModule,
@@ -27,4 +28,5 @@ import { FooterComponent } from './components/footer/footer.component';
   providers: [GlobalService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import {Routes} from '@angular/router';
 import {LaunchComponent} from "./views/launch/launch.component";
 import {HomeComponent} from "./views/home/home.component";
+import {RecognizeComponent} from "./views/recognize/recognize.component";
 
 export const ROUTES: Routes = [
   {path: '', component: LaunchComponent},
   {path: 'home', component: HomeComponent},
+  {path: 'recognize', component: RecognizeComponent},
   {path: '**', redirectTo: ''}
 ];
